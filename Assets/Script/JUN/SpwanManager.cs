a9e8325c52faec84758c6dfb4ca337bfbc7f8856
Junsik-Yoon <epsqjaos@naver.com>
2022-04-29 00:08:13 +0900

    mk4
    mk4

--- Assets/Script/JUN/SpwanManager.cs
+++ Assets/Script/JUN/SpwanManager.cs
@@ -19,8 +19,13 @@ public class SpwanManager : MonoBehaviour
         DontDestroyOnLoad(gameObject);
         _instance = this;
     }
-    public GameObject encounter;
-    public GameObject encounter2;
+    public GameObject encounterDracula;
+    public GameObject encounterHorse;
+
+    public GameObject encounterDruid;
+    public GameObject encounterOgre;
+    public GameObject encounterArcher;
+    public GameObject encounterMagician;
     int randomX; //적이 나타날 X좌표를 랜덤으로 생성해 줍니다.
     int randomY; 
 
@@ -51,15 +56,37 @@ public class SpwanManager : MonoBehaviour
         IEnumerator SpawnWait()
         {
             yield return new WaitForSeconds(3.0f);
+            Vector2 randomVector;
             for(int i=27; i<31; ++i)
             {   
                 randomX = Random.Range(9, 14); 
                 randomY = i;
-                Vector2 randomVector = new Vector2(randomX,randomY);
-                spawnPoints.Add(Instantiate(encounter, randomVector , Quaternion.identity));
+                randomVector = new Vector2(randomX,randomY);
+                spawnPoints.Add(Instantiate(encounterDracula, randomVector , Quaternion.identity));
                 randomX = Random.Range(-4, 1); 
                 randomVector = new Vector2(randomX,randomY);
-                spawnPoints.Add(Instantiate(encounter2, randomVector , Quaternion.identity));
+                spawnPoints.Add(Instantiate(encounterHorse, randomVector , Quaternion.identity));
             }
+             for(int i=32; i<36; ++i)
+             {
+                randomX = Random.Range(9, 14); 
+                randomY = i;
+                randomVector = new Vector2(randomX,randomY);
+                spawnPoints.Add(Instantiate(encounterDruid, randomVector , Quaternion.identity));
+                randomX = Random.Range(-4, 1); 
+                randomVector = new Vector2(randomX,randomY);
+                spawnPoints.Add(Instantiate(encounterArcher, randomVector , Quaternion.identity));
+             }
+
+            for(int i=22; i<26; ++i)
+            {
+                randomX = Random.Range(9, 14); 
+                randomY = i;
+                randomVector = new Vector2(randomX,randomY);
+                spawnPoints.Add(Instantiate(encounterOgre, randomVector , Quaternion.identity));
+                randomX = Random.Range(-4, 1); 
+                randomVector = new Vector2(randomX,randomY);
+                spawnPoints.Add(Instantiate(encounterMagician, randomVector , Quaternion.identity));
+             }
         }
 }
